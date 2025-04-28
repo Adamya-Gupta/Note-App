e492ed18dc233aa55f7e24792494849624410b64
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-04-29 05:20:01 +0530

    task to files

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ app.use(express.static(path.join(__dirname,"pubilc")))
 app.get('/',(req,res)=>{
     fs.readdir(`./files`,(err,files)=>{
         res.render("index" ,{files:files})
-    })
-    
+    })   
 })
+
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        res.redirect("/")
+    })
+})
+
 
 app.listen(3000)
