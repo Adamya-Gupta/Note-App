4d8ea30d4f929cb1b9b0c2e43b213ee91d3cd4aa
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-04-29 19:45:04 +0530

    AWS deploy ready

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express')
 const app = express();
 const path = require('path')
@@ -49,6 +51,6 @@ app.get('/delete/:filename', (req, res) => {
     });
 })
 
+const PORT = process.env.PORT || 3000
 
-
-app.listen(3000)
+app.listen(PORT)
